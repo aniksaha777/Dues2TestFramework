b6ca2419d60fbc9f2967cc39e0d64f9e4570424c
Anik <Anik@SAMMSA-ANIK>
2020-10-19 13:47:38 +0530

    Update Code - 19.10.2020

--- MemberDues2.0/src/test/java/com/memberdues/pages/FirstResetPasswordPage.java
+++ MemberDues2.0/src/test/java/com/memberdues/pages/FirstResetPasswordPage.java
@@ -1,11 +1,8 @@
 package com.memberdues.pages;
-import javax.swing.text.Highlighter;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.Assert;
 import com.memberdues.utility.Helper;
 
 public class FirstResetPasswordPage{
@@ -30,8 +27,8 @@ public class FirstResetPasswordPage{
 	@FindBy(xpath = "//*[@id='confirmationModalBody']/h6") WebElement ConfirmTextOnModal;
 	@FindBy(xpath = "//*[@id='confirmationModalCloseBtn']") WebElement ModalClose;
 	@FindBy(xpath = "//*[@id='confirmationModalYesBtn']") WebElement ModalYes;
-	
-	
+
+
 
 	public String get_First_Reset_Header() {
 
@@ -41,48 +38,65 @@ public class FirstResetPasswordPage{
 	}
 
 
-	public void reset_First_Password(String OldPw, String newpw, String Q1Val, String Ans1, String Q2Val, String Ans2) {
-		
+	public void reset_Details(String OldPw, String newpw, String Q1Val, String Ans1, String Q2Val, String Ans2) {
+
 		Helper.highlightElement(driver, OldPassword);
 		OldPassword.sendKeys(OldPw);
-		
+
 		Helper.highlightElement(driver, NewPassword);
 		NewPassword.sendKeys(newpw);
-		
+
 		Helper.highlightElement(driver, ConfirmPassword);
 		ConfirmPassword.sendKeys(newpw);
-		
+
 		Helper.highlightElement(driver, Question1);
 		Select Ques1 = new Select(Question1);
 		Ques1.selectByValue(Q1Val);
-		
+
 		Helper.highlightElement(driver, Answer1);
 		Answer1.sendKeys(Ans1);
-		
+
+		Helper.highlightElement(driver, Question2);
 		Select Ques2 = new Select(Question2);
 		Ques2.selectByValue(Q2Val);
-		
+
 		Helper.highlightElement(driver, Answer2);
 		Answer2.sendKeys(Ans2);
-		
+
+	}
+
+	public void click_Submit() {
 		Helper.highlightElement(driver, Submit);
 		Submit.click();
-		
 	}
-	
+
 	public String confirm_Text_Modal() {
 		Helper.highlightElement(driver, ConfirmModal);
 		Helper.highlightElement(driver, ConfirmTextOnModal);
 		String Modal_text = ConfirmTextOnModal.getText();
 		return Modal_text;
 	}
-	
+
 	public void Confirm_Reset() {
-		
+
 		Helper.highlightElement(driver, ModalClose);
 		Helper.highlightElement(driver, ModalYes);
 		ModalYes.click();
-		
+
 	}
 
+	public boolean isResetHeaderPresent() {
+		try {
+			Thread.sleep(1000);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.getMessage();
+		}
+		if(ResetPwHeader.isDisplayed()) {
+			return true; 
+		}
+		else {
+			return false;
+		}
+	}
 }
--- MemberDues2.0/src/test/java/com/memberdues/testcases/LoginTests.java
+++ MemberDues2.0/src/test/java/com/memberdues/testcases/LoginTests.java
@@ -10,31 +10,39 @@ import com.memberdues.utility.Helper;
 
 public class LoginTests extends BaseClass {
 
-	@Test(enabled = false)
+	@Test(priority = 0)
 	public void Login_to_Member_Portal() {
 
+		// Get Methodname . Methodname = testcasename
 		String testcasename = Thread.currentThread().getStackTrace()[1].getMethodName();
-		int datarow = excel.GetDataRowNum(testcasename);
-		// System.out.println("Data Row is ="+datarow);
 
+		// Fetch Data Row and Columm number from Test Data
+		int datarow = excel.GetDataRowNum(testcasename);
 		int unamecol = excel.GetDataColNum("Username");
 		int pwcol = excel.GetDataColNum("Password");
 		int tokencol = excel.GetDataColNum("Token");
 
-		logger = report.createTest("Login to Member Portal");
+		// Fetch Test Data Excel
+		String uname = excel.getStringData(datarow, unamecol);
+		String pw = excel.getStringData(datarow, pwcol);
+		String token = excel.getStringData(datarow, tokencol);
 
+		logger = report.createTest("Login to Member Portal");
 		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+		// Start Application
 		logger.info("Starting Application");
 		Helper.captureScreenshot(driver, testcasename);
-		loginPage.Login_to_Member_Portal(excel.getStringData(datarow, unamecol), excel.getStringData(datarow, pwcol),
-				excel.getStringData(datarow, tokencol));
+
+		loginPage.Login_to_Member_Portal(uname, pw, token);
+		logger.pass("Login Details Entered. Login button Clicked.");
 		Helper.captureScreenshot(driver, testcasename);
 		logger.pass("Login is Successful");
 	}
 
-	@Test(enabled = false)
+	@Test(priority=1)
 	public void Logout_of_Member_Portal() {
 		String testcasename = Thread.currentThread().getStackTrace()[1].getMethodName();
+
 		int datarow = excel.GetDataRowNum(testcasename);
 
 		int unamecol = excel.GetDataColNum("Username");
@@ -55,10 +63,13 @@ public class LoginTests extends BaseClass {
 
 	}
 
-	@Test(enabled = false)
+	@Test(priority=9)
 	public void First_Time_Reset_Password() {
 
+		// Get Methodname . Methodname = testcasename
 		String testcasename = Thread.currentThread().getStackTrace()[1].getMethodName();
+
+		// Fetch Data Row and Columm number from Test Data
 		int datarow = excel.GetDataRowNum(testcasename);
 
 		int unamecol = excel.GetDataColNum("Username");
@@ -70,47 +81,93 @@ public class LoginTests extends BaseClass {
 		int Q1Acol = excel.GetDataColNum("Q1Ans");
 		int Q2Vcol = excel.GetDataColNum("Q2Val");
 		int Q2Acol = excel.GetDataColNum("Q2Ans");
+		int Modaltextcol = excel.GetDataColNum("ExpectedString");
 
-		logger = report.createTest("First Reset Password for Member");
-
-		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
-		FirstResetPasswordPage FirstResetPasswordPage = PageFactory.initElements(driver, FirstResetPasswordPage.class);
-		logger.info("Starting Application");
-		Helper.captureScreenshot(driver, testcasename);
-		loginPage.Login_to_Member_Portal(excel.getStringData(datarow, unamecol), excel.getStringData(datarow, pwcol),
-				excel.getStringData(datarow, tokencol));
-		Helper.captureScreenshot(driver, testcasename);
-		String Actual = FirstResetPasswordPage.get_First_Reset_Header();
-		String Expected = excel.getStringData(datarow, rhcol);
-		Assert.assertEquals(Actual, Expected);
-		System.out.println("Reset Password Page Header Matched");
-		Helper.captureScreenshot(driver, testcasename);
+		// Fetch Test Data from Excel
+		String uname = excel.getStringData(datarow, unamecol);
+		String pw = excel.getStringData(datarow, pwcol);
+		String token = excel.getStringData(datarow, tokencol);
 		String newpass = excel.getStringData(datarow, npcol);
 		String oldpass = excel.getStringData(datarow, pwcol);
 		String Q1value = excel.getStringData(datarow, Q1Vcol);
 		String Q1Ans = excel.getStringData(datarow, Q1Acol);
 		String Q2value = excel.getStringData(datarow, Q2Vcol);
 		String Q2Ans = excel.getStringData(datarow, Q2Acol);
+		String ModalTextExpected = excel.getStringData(datarow, Modaltextcol);
 
-		FirstResetPasswordPage.reset_First_Password(oldpass, newpass, Q1value, Q1Ans, Q2value, Q2Ans);
-		Helper.captureScreenshot(driver, testcasename);
-		String ModalTextActual = FirstResetPasswordPage.confirm_Text_Modal();
-		String ModalTextExpected = "Are you sure to reset the password and set the security questions?";
-		Assert.assertEquals(ModalTextActual, ModalTextExpected);
-		System.out.println("Confirm Modal Matched");
-		Helper.captureScreenshot(driver, testcasename);
-		FirstResetPasswordPage.Confirm_Reset();
+		logger = report.createTest("First Reset Password for Member");
 
-		loginPage.Login_to_Member_Portal(excel.getStringData(datarow, unamecol), newpass,
-				excel.getStringData(datarow, 3));
+		// Initializing Factory Pages
+		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+		FirstResetPasswordPage FirstResetPasswordPage = PageFactory.initElements(driver, FirstResetPasswordPage.class);
+
+		// Starting Application
+		logger.info("Starting Application");
 		Helper.captureScreenshot(driver, testcasename);
-		loginPage.Logout_of_Member_Portal();
+		loginPage.Login_to_Member_Portal(uname, pw, token);
+		logger.pass("Login Details Entered. Login Button Clicked");
 		Helper.captureScreenshot(driver, testcasename);
-		logger.pass("First time Reset Password is Successful");
+		String Actual = FirstResetPasswordPage.get_First_Reset_Header();
+		String Expected = excel.getStringData(datarow, rhcol);
+		if (FirstResetPasswordPage.isResetHeaderPresent()) {
+			if (Actual.equals(Expected)) {
+				System.out.println("First Reset Password Page Header Matched");
+				logger.pass("First Reset Password Page Header Matched");
+				Helper.captureScreenshot(driver, testcasename);
+				FirstResetPasswordPage.reset_Details(oldpass, newpass, Q1value, Q1Ans, Q2value, Q2Ans);
+				Helper.captureScreenshot(driver, testcasename);
+				logger.pass("Reset Details Entered");
+				System.out.println("Reset Details Entered");
+				FirstResetPasswordPage.click_Submit();
+				Helper.captureScreenshot(driver, testcasename);
+				logger.pass("Reset Password Button Clicked");
+				System.out.println("Reset Password Button Clicked");
+
+				String ModalTextActual = FirstResetPasswordPage.confirm_Text_Modal();
+				if (ModalTextExpected.equals(ModalTextActual)) {
+
+					System.out.println("Confirm Reset Password Modal text Matched");
+					logger.pass("Confirm Reset Password Modal text Matched");
+					Helper.captureScreenshot(driver, testcasename);
+					FirstResetPasswordPage.Confirm_Reset();
+
+					System.out.println("Confirm Reset Password Modal Accepted");
+					logger.pass("Confirm Reset Password Modal Accepted");
+
+					loginPage.Login_to_Member_Portal(uname, newpass, token);
+					System.out.println("Login Details Entered. Login Button Clicked");
+					logger.pass("Login Details Entered. Login Button Clicked");
+					Helper.captureScreenshot(driver, testcasename);
+
+					loginPage.Logout_of_Member_Portal();
+					Helper.captureScreenshot(driver, testcasename);
+					logger.pass("Login and Logout Successful with new Password");
+
+				}
+
+				else {
+
+					logger.fail("Confirm Reset Password Modal text Not Matched");
+					Assert.fail("Confirm Reset Password Modal text Not Matched");
+
+				}
+			}
+
+			else {
+				logger.fail("User Not Redirected to First Time Reset Password Page.");
+				Assert.fail("User Not Redirected to First Time Reset Password Page.");
+			}
+		}
+
+		else {
+			logger.fail("User Not Redirected to First Time Reset Password Page.");
+			Assert.fail("User Not Redirected to First Time Reset Password Page.");
+
+		}
 
 	}
 
-	@Test(enabled = false)
+	@Test(priority=2)
 	public void Login_Error_On_No_Data() {
 
 		logger = report.createTest("Login Error on Clicking Submit with no Data");
@@ -130,13 +187,13 @@ public class LoginTests extends BaseClass {
 
 	}
 
-	@Test
+	@Test(priority=3)
 	public void Login_Error_On_Wrong_Userid() {
 
 		logger = report.createTest("Login Error on Wrong Username");
 		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
-		
-		//Get Methodname . Methodname = testcasename
+
+		// Get Methodname . Methodname = testcasename
 		String testcasename = Thread.currentThread().getStackTrace()[1].getMethodName();
 
 		// Fetch Data Row and Columm number from Test Data
@@ -144,7 +201,7 @@ public class LoginTests extends BaseClass {
 		int unamecol = excel.GetDataColNum("Username");
 		int pwcol = excel.GetDataColNum("Password");
 		int tokencol = excel.GetDataColNum("Token");
-		int errorcol = excel.GetDataColNum("ExpectedError");
+		int errorcol = excel.GetDataColNum("ExpectedString");
 
 		// Fetch Test Data Excel
 		String uname = excel.getStringData(datarow, unamecol);
@@ -166,7 +223,7 @@ public class LoginTests extends BaseClass {
 			loginPage.handleAlert();
 			logger.info("Alert is Accepted");
 			Helper.captureScreenshot(driver, testcasename);
-			if (ActualError.equals(ExpectedError)) {				
+			if (ActualError.equals(ExpectedError)) {
 				System.out.println("Alert text for Incorrect Credentials Matched");
 				logger.pass("Alert text for Incorrect Credentials Matched");
 
@@ -178,12 +235,11 @@ public class LoginTests extends BaseClass {
 					Assert.fail("User Not redirected to Login Page");
 					e.getMessage();
 				}
-				
-			}
-			else {
+
+			} else {
 				logger.fail("Expected Alert is not Matched.");
 				Assert.fail("Expected Alert is not Matched.");
-				
+
 			}
 
 		} else {
@@ -192,13 +248,13 @@ public class LoginTests extends BaseClass {
 		}
 	}
 
-	@Test
+	@Test(priority=4)
 	public void Login_Error_On_Wrong_Password() {
-		
+
 		logger = report.createTest("Login Error on Wrong Password");
 		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
-		
-		//Get Methodname . Methodname = testcasename
+
+		// Get Methodname . Methodname = testcasename
 		String testcasename = Thread.currentThread().getStackTrace()[1].getMethodName();
 
 		// Fetch Data Row and Columm number from Test Data
@@ -206,7 +262,7 @@ public class LoginTests extends BaseClass {
 		int unamecol = excel.GetDataColNum("Username");
 		int pwcol = excel.GetDataColNum("Password");
 		int tokencol = excel.GetDataColNum("Token");
-		int errorcol = excel.GetDataColNum("ExpectedError");
+		int errorcol = excel.GetDataColNum("ExpectedString");
 
 		// Fetch Test Data Excel
 		String uname = excel.getStringData(datarow, unamecol);
@@ -228,7 +284,7 @@ public class LoginTests extends BaseClass {
 			loginPage.handleAlert();
 			logger.info("Alert is Accepted");
 			Helper.captureScreenshot(driver, testcasename);
-			if (ActualError.equals(ExpectedError)) {				
+			if (ActualError.equals(ExpectedError)) {
 				System.out.println("Alert text for Incorrect Credentials Matched");
 				logger.pass("Alert text for Incorrect Credentials Matched");
 
@@ -240,12 +296,11 @@ public class LoginTests extends BaseClass {
 					Assert.fail("User Not redirected to Login Page");
 					e.getMessage();
 				}
-				
-			}
-			else {
+
+			} else {
 				logger.fail("Expected Alert is not Matched.");
 				Assert.fail("Expected Alert is not Matched.");
-				
+
 			}
 
 		} else {
@@ -254,13 +309,13 @@ public class LoginTests extends BaseClass {
 		}
 	}
 
-	@Test
+	@Test(priority=5)
 	public void Login_Error_On_Wrong_Token() {
 
 		logger = report.createTest("Login Error on Wrong Token");
 		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
-		
-		//Get Methodname . Methodname = testcasename
+
+		// Get Methodname . Methodname = testcasename
 		String testcasename = Thread.currentThread().getStackTrace()[1].getMethodName();
 
 		// Fetch Data Row and Columm number from Test Data
@@ -268,7 +323,7 @@ public class LoginTests extends BaseClass {
 		int unamecol = excel.GetDataColNum("Username");
 		int pwcol = excel.GetDataColNum("Password");
 		int tokencol = excel.GetDataColNum("Token");
-		int errorcol = excel.GetDataColNum("ExpectedError");
+		int errorcol = excel.GetDataColNum("ExpectedString");
 
 		// Fetch Test Data Excel
 		String uname = excel.getStringData(datarow, unamecol);
@@ -290,7 +345,7 @@ public class LoginTests extends BaseClass {
 			loginPage.handleAlert();
 			logger.info("Alert is Accepted");
 			Helper.captureScreenshot(driver, testcasename);
-			if (ActualError.equals(ExpectedError)) {				
+			if (ActualError.equals(ExpectedError)) {
 				System.out.println("Alert text for Incorrect Token Matched");
 				logger.pass("Alert text for Incorrect Token Matched");
 
@@ -302,12 +357,11 @@ public class LoginTests extends BaseClass {
 					Assert.fail("User Not redirected to Login Page");
 					e.getMessage();
 				}
-				
-			}
-			else {
+
+			} else {
 				logger.fail("Expected Alert is not Matched.");
 				Assert.fail("Expected Alert is not Matched.");
-				
+
 			}
 
 		} else {
@@ -315,4 +369,195 @@ public class LoginTests extends BaseClass {
 			Assert.fail("Expected Alert is not displayed.");
 		}
 	}
+
+	@Test(priority=6)
+	public void Login_Error_On_AppAccess_Restricted() {
+
+		logger = report.createTest("Login Error when Member Application Access Restricted");
+		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+
+		// Get Methodname . Methodname = testcasename
+		String testcasename = Thread.currentThread().getStackTrace()[1].getMethodName();
+
+		// Fetch Data Row and Columm number from Test Data
+		int datarow = excel.GetDataRowNum(testcasename);
+		int unamecol = excel.GetDataColNum("Username");
+		int pwcol = excel.GetDataColNum("Password");
+		int tokencol = excel.GetDataColNum("Token");
+		int errorcol = excel.GetDataColNum("ExpectedString");
+
+		// Fetch Test Data Excel
+		String uname = excel.getStringData(datarow, unamecol);
+		String pw = excel.getStringData(datarow, pwcol);
+		String token = excel.getStringData(datarow, tokencol);
+		String ExpectedError = excel.getStringData(datarow, errorcol);
+
+		// Start Application
+		logger.info("Starting Application");
+
+		loginPage.Login_to_Member_Portal(uname, pw, token);
+		logger.pass("Login Details Entered.Login Button Clicked");
+
+		if (loginPage.isAlertPresent()) {
+			System.out.println("Alert is present");
+			logger.pass("Alert is present");
+			String ActualError = loginPage.getAlertText();
+			System.out.println("Expected Error: " + ExpectedError);
+			System.out.println("Actual Error: " + ActualError);
+			loginPage.handleAlert();
+			logger.info("Alert is Accepted");
+			Helper.captureScreenshot(driver, testcasename);
+			if (ActualError.equals(ExpectedError)) {
+				System.out.println("Alert text for Member App Access Restricted Matched");
+
+				try {
+					loginPage.verifyLoginPageHeader();
+					logger.pass("Login Page Redirection Verified");
+				} catch (Exception e) {
+					logger.fail("User Not redirected to Login Page");
+					Assert.fail("User Not redirected to Login Page");
+					e.getMessage();
+				}
+
+			} else {
+				logger.fail("Expected Alert is not Matched.");
+				Assert.fail("Expected Alert is not Matched.");
+
+			}
+
+		} else {
+			logger.fail("Expected Alert is not displayed.");
+			Assert.fail("Expected Alert is not displayed.");
+		}
+
+	}
+
+	@Test(priority=7)
+	public void Login_Error_On_Member_LoginRestricted() {
+
+		logger = report.createTest("Login Error when Member Login Access Restricted");
+		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+
+		// Get Methodname . Methodname = testcasename
+		String testcasename = Thread.currentThread().getStackTrace()[1].getMethodName();
+
+		// Fetch Data Row and Columm number from Test Data
+		int datarow = excel.GetDataRowNum(testcasename);
+		int unamecol = excel.GetDataColNum("Username");
+		int pwcol = excel.GetDataColNum("Password");
+		int tokencol = excel.GetDataColNum("Token");
+		int errorcol = excel.GetDataColNum("ExpectedString");
+
+		// Fetch Test Data Excel
+		String uname = excel.getStringData(datarow, unamecol);
+		String pw = excel.getStringData(datarow, pwcol);
+		String token = excel.getStringData(datarow, tokencol);
+		String ExpectedError = excel.getStringData(datarow, errorcol);
+
+		// Start Application
+		logger.info("Starting Application");
+
+		loginPage.Login_to_Member_Portal(uname, pw, token);
+		logger.pass("Login Details Entered.Login Button Clicked");
+
+		if (loginPage.isAlertPresent()) {
+			System.out.println("Alert is present");
+			logger.pass("Alert is present");
+			String ActualError = loginPage.getAlertText();
+			System.out.println("Expected Error: " + ExpectedError);
+			System.out.println("Actual Error: " + ActualError);
+			loginPage.handleAlert();
+			logger.info("Alert is Accepted");
+			Helper.captureScreenshot(driver, testcasename);
+			if (ActualError.equals(ExpectedError)) {
+				System.out.println("Alert text for Member Login Access Restricted Matched");
+				logger.pass("Alert text for Member Login Access Restricted Matched");
+
+				try {
+					loginPage.verifyLoginPageHeader();
+					logger.pass("Login Page Redirection Verified");
+				} catch (Exception e) {
+					logger.fail("User Not redirected to Login Page");
+					Assert.fail("User Not redirected to Login Page");
+					e.getMessage();
+				}
+
+			} else {
+				logger.fail("Expected Alert is not Matched.");
+				Assert.fail("Expected Alert is not Matched.");
+
+			}
+
+		} else {
+			logger.fail("Expected Alert is not displayed.");
+			Assert.fail("Expected Alert is not displayed.");
+		}
+
+	}
+
+	@Test(priority=8)
+	public void Login_Error_On_Member_Deleted() {
+
+		logger = report.createTest("Login Error when Member is Deleted");
+		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+
+		// Get Methodname . Methodname = testcasename
+		String testcasename = Thread.currentThread().getStackTrace()[1].getMethodName();
+
+		// Fetch Data Row and Columm number from Test Data
+		int datarow = excel.GetDataRowNum(testcasename);
+		int unamecol = excel.GetDataColNum("Username");
+		int pwcol = excel.GetDataColNum("Password");
+		int tokencol = excel.GetDataColNum("Token");
+		int errorcol = excel.GetDataColNum("ExpectedString");
+
+		// Fetch Test Data Excel
+		String uname = excel.getStringData(datarow, unamecol);
+		String pw = excel.getStringData(datarow, pwcol);
+		String token = excel.getStringData(datarow, tokencol);
+		String ExpectedError = excel.getStringData(datarow, errorcol);
+
+		// Start Application
+		logger.info("Starting Application");
+
+		loginPage.Login_to_Member_Portal(uname, pw, token);
+		logger.pass("Login Details Entered.Login Button Clicked");
+
+		if (loginPage.isAlertPresent()) {
+			System.out.println("Alert is present");
+			logger.pass("Alert is present");
+			String ActualError = loginPage.getAlertText();
+			System.out.println("Expected Error: " + ExpectedError);
+			System.out.println("Actual Error: " + ActualError);
+			loginPage.handleAlert();
+			logger.info("Alert is Accepted");
+			Helper.captureScreenshot(driver, testcasename);
+			if (ActualError.equals(ExpectedError)) {
+				System.out.println("Alert text for Member is Deleted Matched");
+				logger.pass("Alert text for Member is Deleted Matched");
+
+				try {
+					loginPage.verifyLoginPageHeader();
+					logger.pass("Login Page Redirection Verified");
+				} catch (Exception e) {
+					logger.fail("User Not redirected to Login Page");
+					Assert.fail("User Not redirected to Login Page");
+					e.getMessage();
+				}
+
+			} else {
+				logger.fail("Expected Alert is not Matched.");
+				Assert.fail("Expected Alert is not Matched.");
+
+			}
+
+		} else {
+			logger.fail("Expected Alert is not displayed.");
+			Assert.fail("Expected Alert is not displayed.");
+		}
+
+	}
+
+	
+	
 }
